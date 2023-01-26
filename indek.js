f6303ec0b7516505616547ef23b490d9dc1793a6
Faishal Hadi Rustandi <123096711+shaldi1710@users.noreply.github.com>
2023-01-26 15:09:31 +0700

    Add files via upload

---
+++ indek.js
@@ -0,0 +1,20 @@
+var tanggal = 23;
+var bulan = 1;
+var tahun = 2001;
+
+switch (bulan) {
+  case 1: { konversiBulan = 'januari'; break; }
+  case 2: { conversiBulan = 'februari'; break; }
+  case 3: { conversiBulan = 'maret'; break; }
+  case 4: { conversiBulan = 'april'; break; }
+  case 4: { conversiBulan = 'mei'; break; }
+  case 4: { conversiBulan = 'juni'; break; }
+  case 4: { conversiBulan = 'juli'; break; }
+  case 4: { conversiBulan = 'agustus'; break; }
+  case 4: { conversiBulan = 'september'; break; }
+  case 4: { conversiBulan = 'oktober'; break; }
+  case 4: { conversiBulan = 'november'; break; }
+  case 4: { conversiBulan = 'desember'; break; }
+  default: { console.log('Tidak terjadi apa-apa'); }
+}
+console.log(tanggal, konversiBulan, tahun);
---
+++ tugas java.js
@@ -0,0 +1,24 @@
+var nama = '';
+var peran = '';
+if (nama === 'mikael' && peran === '') {
+  console.log("halo Mikael, Pilih peranmu untuk memulai game!")
+}
+
+else if (nama === 'Nina' && peran === 'Ksatria') {
+  console.log("Selamat datang di Dunia Proxytia, Nina")
+  console.log("Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!")
+}
+
+else if (nama === 'Danu' && peran === 'Tabib') {
+  console.log("Selamat datang di Dunia Proxytia, Danu")
+  console.log("Halo Tabib Danu, kamu akan membantu temanmu yang terluka.")
+}
+
+else if (nama === 'Zero' && peran === 'Penyihir') {
+  console.log("Selamat datang di Dunia Proxytia, Zero")
+  console.log("Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!")
+}
+
+else {
+  console.log("nama harus diisi")
+}
